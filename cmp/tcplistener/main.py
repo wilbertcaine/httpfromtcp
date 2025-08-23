92a7597e65ef8623929155d02ae17560d0421bc8
Wilbert Caine <wcaine@ust.hk>
2025-08-24 06:09:33 +0800

    fix tcplistener to use os.read

--- cmp/tcplistener/main.py
+++ cmp/tcplistener/main.py
@@ -1,15 +1,9 @@
 import os
 import socket
 
-def get_lines_address(address):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(address)
-    s.listen()
-    conn, addr = s.accept()
-    print(f'addr={addr}')
-
+def get_lines_channel(fd):
     curr = ''
-    while data := conn.recv(8):
+    while data := os.read(fd, 8):
         data = data.decode()
         data = data.split('\n')
         while len(data) > 1:
@@ -17,10 +11,15 @@ def get_lines_address(address):
             yield curr
             curr = ''
         curr += data.pop(0)
-    s.close()
+    os.close(fd)
 
 
 if __name__ == "__main__":
-    for line in get_lines_address(('localhost', 42069)):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', 42069))
+    s.listen()
+    conn, addr = s.accept()
+    print(f'addr={addr}')
+    for line in get_lines_channel(conn.fileno()):
         print(line)
 
